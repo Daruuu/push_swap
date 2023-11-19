e0f7e4eec4f2ccb25c8ad1fc01cd397fc8ff8c66
Daruny Salazar Cardoza <dasalaza@car14s5.42barcelona.com>
2023-11-19 18:01:15 +0100

    upd basic

--- ft_main.c
+++ ft_main.c
@@ -6,32 +6,30 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 20:01:12 by dasalaza          #+#    #+#             */
-/*   Updated: 2023/11/19 03:20:46 by dasalaza         ###   ########.fr       */
+/*   Updated: 2023/11/19 17:53:48 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	check_duplicate_number(char **argv)
+int	check_duplicate_number(int argc, char **argv)
 {
-
-
-	/*
 	int	i;
 	int	j;
 
-	i = 0;
-	j = 1;
-
-	while (argv[1][i] != '\0')
+	i = 1;
+	while (i < argc)
 	{
-		if (!(argv[1][i] == argv[1][j]))
-			return(0);
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
+				return (1);
+			j++;
+		}
 		i++;
-		j++;
 	}
-	*/
-	return (1);
+	return (0);
 }
 
 int	check_range_number(char *argv[])
@@ -42,23 +40,11 @@ int	check_range_number(char *argv[])
 	while (argv[1][i] != '\0')
 	{
 		if (argv[1][i] < '0' && argv[1][i] > '9') 
-		{
 			return (0);
-		}
 		i++;
 	}
 	return (1);
 }
-
-/*
- * example input:
- * CASE 1:
- * ./push_swap 1 3 5 6 7
- *
- * CASE 2:
- * ARG="4 1 9 2"./push_swap $ARG | wc -l
- *
- */
 
 void	check_all_input(int argc, char *argv[])
 {
@@ -71,13 +57,24 @@ void	check_all_input(int argc, char *argv[])
 	{
 		//funcion comprobar que no se repitan los numeros
 		//if (check_range_number(argv) || check_duplicate_number(argv))
-		if (check_duplicate_number(argv))
+		if (check_duplicate_number(argc, argv))
+		//if (check_range_number(argv))
 			write(1, "valid input\n", 12);
 		else
 			write(1, "invalid input\n", 14);
 	}
 }
 
+
+/*
+ * example input:
+ * CASE 1:
+ * ./push_swap 1 3 5 6 7
+ *
+ * CASE 2:
+ * ARG="4 1 9 2"./push_swap $ARG | wc -l
+ *
+ */
 int	main(int argc, char *argv[])
 {
 	check_all_input(argc, argv);
--- push_swap.h
+++ push_swap.h
@@ -6,18 +6,29 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 23:58:23 by dasalaza          #+#    #+#             */
-/*   Updated: 2023/11/19 02:33:12 by dasalaza         ###   ########.fr       */
+/*   Updated: 2023/11/19 17:51:38 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <limits.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <libft.h>
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "srcs/src_libft/libft.h"
 
+typedef struct s_list
+{
+	int value;
+	int index;
+	struct s_list *next;
+}		t_list;
+
+typedef struct s_stack
+{
+	int len;
+	struct s_list *first;
+}		t_stack;	
 
 #endif
