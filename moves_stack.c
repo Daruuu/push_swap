f40d7d304644f4e5dfe72c07fdbc43c4b473d347
Daruny Salazar Cardoza <dasalaza@car9s1.42barcelona.com>
2023-12-10 02:12:34 +0100

    moves stack starting

--- moves_stack.c
+++ moves_stack.c
@@ -6,10 +6,14 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 19:50:48 by dasalaza          #+#    #+#             */
-/*   Updated: 2023/12/06 20:37:42 by dasalaza         ###   ########.fr       */
+/*   Updated: 2023/12/10 02:12:12 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+/*
+ * function to order 3 random number in min movements
+*/
+void	:wq
 
