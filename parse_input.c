0c13ab4e94a488cc24b4178df2298b546c0f7d29
Daruuu <thedaruny16@gmail.com>
2023-12-31 11:23:31 +0100

    add home

--- parse_input.c
+++ parse_input.c
@@ -6,7 +6,7 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 19:53:30 by dasalaza          #+#    #+#             */
-/*   Updated: 2023/12/10 20:38:29 by dasalaza         ###   ########.fr       */
+/*   Updated: 2023/12/30 15:59:00 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,28 @@ void	print_stack(int argc, char **argv)
 
 	stack_a = fill_linked_list (argc, argv);
 	t_node	*current = stack_a.first;
+
+	while (current != NULL)
+	{
+		current->index, current->value
+		current = current->next;
+		printf("index: [%d] -> value: %d\n", current->index, current->value);
+	}
+}
+/*
+void	print_stack(int argc, char **argv)
+{
+	t_stack	stack_a;
+
+	stack_a = fill_linked_list (argc, argv);
+	t_node	*current = stack_a.first;
 	while (current != NULL)
 	{
 		printf("index: [%d] -> value: %d\n", current->index, current->value);
 		current = current->next;
 	}
 }
+*/
 
 /*
  * funct to fill stack a with argv
