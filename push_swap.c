f283f6e38268a2d54f9b7a7a0b9637f25c0af665
Daruny Salazar Cardoza <dasalaza@cbr9s5.42barcelona.com>
2024-01-10 18:03:13 +0100

    push juanary 10

--- push_swap.c
+++ push_swap.c
@@ -6,9 +6,21 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 23:58:37 by dasalaza          #+#    #+#             */
-/*   Updated: 2023/11/19 16:39:08 by dasalaza         ###   ########.fr       */
+/*   Updated: 2024/01/10 17:19:37 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+/*
+int	main(int ac, char **av)
+{
+	t_stack	*a;
+	t_stack	*b;
+
+	a = NULL;
+	b = NULL;
+
+	return(0);
+}
+*/
--- types_of_moves.c
+++ types_of_moves.c
@@ -6,7 +6,7 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 19:36:51 by dasalaza          #+#    #+#             */
-/*   Updated: 2024/01/05 23:03:49 by dasalaza         ###   ########.fr       */
+/*   Updated: 2024/01/09 20:37:24 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,11 @@
 //SA
 //swap between 2 nodes
 
-void	swap_a(t_node **head_ref)
+void	swap_a_(t_node **head_ref)
 {
-	int	counter;
+	int	tmp;
 
-	counter = 0;
 	if (*head_ref == NULL)
-	{
-		write(1,"error\n", 6);
-		counter++;
-
-	}
+		return ;
 
 }
