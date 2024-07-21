484f6e68a44c069c363a702eacdf81ab91adcfbf
Daruuu <thedaruny16@gmail.com>
2024-07-21 19:07:31 +0200

    firs from 42 campus

--- push_swap.c
+++ push_swap.c
@@ -6,37 +6,38 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 23:58:37 by dasalaza          #+#    #+#             */
-/*   Updated: 2024/07/06 12:36:46 by dasalaza         ###   ########.fr       */
+/*   Updated: 2024/07/21 18:17:35 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/*
- * example input:
- * CASE 1:
- * ./push_swap 1 3 5 6 7
- *
- * CASE 2:
- * ARG="4 1 9 2"./push_swap $ARG | wc -l
-*/
-
-/*
 int	main(int ac, char **av)
 {
-	check_all_inputs(ac, av);
-	//print_stack(ac, av);
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+	char	**new_arg;
+	int		len_new_ac;
+
+	stack_a = NULL;
+	stack_b = NULL;
+	new_arg = NULL;
+	if (ac < 2)
+		return (0);
+	if (ac == 2)
+	{
+		new_arg = ft_split(av[1], ' ');
+		len_new_ac = 0;
+		while (new_arg[len_new_ac])
+			len_new_ac ++;
+		ac = len_new_ac;
+	}
+	input_check_argc(ac, av);
+	stack_a = init_stack(ac, av);
+	stack_b = init_stack_empty();
+	set_index_stack(stack_a);
+	handle_sort_options(&stack_a, &stack_b, stack_a->len);
+	free_allocs(ac, stack_a, stack_b, new_arg);
 	return (0);
 }
-*/
-/*
-    t_stack *stack_a;
-	stack_a = fill_t_list(argc, argv);
-
-    t_list *current = stack_a.first;
-    while (current != NULL)
-    {
-        printf("Index: %d -> value: [%d]\n", current->index, current->value);
-        urrent = current->next;
-    }
-*/
+//	print_stack(stack_a);
